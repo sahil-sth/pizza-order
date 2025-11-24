8ca719fa456a6c5150e26bee6cfd4e79c455d1e4
sahil-sth <saheel.sth99@gmail.com>
2025-11-23 23:20:51 -0700

    Add colors to the font and background

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,8 +3,10 @@ import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Pizza App</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Pizza App
+      </Link>
       <SearchOrder />
       <p>Menu</p>
     </header>
