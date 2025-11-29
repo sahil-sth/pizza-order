b1c4f2d3bf166e85d5e08e35cc5edd59dd9fa441
sahil-sth <saheel.sth99@gmail.com>
2025-11-29 13:28:38 -0700

    Style the cart overview

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -6,6 +6,8 @@ function Button({ children, disabled = false, to, type = "primary" }) {
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      "inline-block rounded-full bg-transparent border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
   };
   // if the user sent a to prop means that the user is wanting to use Link
   if (to) {
