7262e8711f0a2eb850820012cd048bacf7985ce8
sahil-sth <saheel.sth99@gmail.com>
2025-12-14 23:41:59 -0700

    Handle the onClick prop to our custom Button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled = false, to, type = "primary" }) {
+function Button({ children, disabled = false, to, type = "primary", onClick }) {
   const base =
     "text-sm inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
   const styles = {
@@ -10,13 +10,19 @@ function Button({ children, disabled = false, to, type = "primary" }) {
       "inline-block rounded-full bg-transparent border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
   };
   // if the user sent a to prop means that the user is wanting to use Link
-  if (to) {
+  if (to)
     return (
       <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
-  }
+
+  if (onClick)
+    return (
+      <button disabled={disabled} className={styles[type]} onClick={onClick}>
+        {children}
+      </button>
+    );
   return (
     <button disabled={disabled} className={styles[type]}>
       {children}
