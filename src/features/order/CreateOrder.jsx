88a808f8bb5fa4857f7f0c0a39b90d8c65b39a02
sahil-sth <saheel.sth99@gmail.com>
2025-12-14 22:42:52 -0700

    CreateOrder element uses username from redux toolkit

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -44,6 +45,7 @@ function CreateOrder() {
   const isSubmitting = navigation.state === "submitting";
   const cart = fakeCart;
   const formErrors = useActionData();
+  const username = useSelector((state) => state.user.username);
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
@@ -51,7 +53,13 @@ function CreateOrder() {
       <Form method="POST" action="">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input type="text" name="customer" className="input grow" required />
+          <input
+            type="text"
+            name="customer"
+            className="input grow"
+            defaultValue={username}
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
