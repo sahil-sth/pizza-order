487b13be68aa485b1f21520d6add596388119d06
sahil-sth <saheel.sth99@gmail.com>
2025-12-21 19:41:07 -0700

    Remove the pizza if the quantity is zero

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -23,9 +23,10 @@ const cartSlice = createSlice({
     },
     decreaseItemQuantity(state, action) {
       const item = state.cart.find((item) => item.pizzaId === action.payload); // find the pizza item
-
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
+      // completely remove the item by calling the deleteItem reducers
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
     clearCart(state) {
       state.cart = [];
